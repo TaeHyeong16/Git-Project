1eb46fbbc9fb941d1576a58eea3f5f15f8fc37ce
TaeHyeong16 <dlxodlxo12@naver.com>
2023-11-24 11:05:16 +0900

    회원가입 추가 완료

--- src/GitMain.java
+++ src/GitMain.java
@@ -2,13 +2,14 @@
 public class GitMain {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		//회원가입 기능 추가 완료
 	}
 	
 	public void join() {
-		//회원가입 수정
+		//회원가입
 	}
 	
 	public void login() {
-		//로그인
+		//로그인 기능
 	}
 }
